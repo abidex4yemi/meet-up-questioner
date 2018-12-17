673810aa0faac410809927625bd2b498393667ea
Sulaiman Hammed Abidemi <abidex4yemi@gmail.com>
2018-12-17 22:52:34 +0100

    Added new UI feature admin dashboard
    As as an admin i should be able to login into my
    dashboard and create or delete meet up and also
    view all activity happening on the website
    
    The new admin dashboard  UI allow admin users
    to priotize top stared question and be able
    to organize the upcoming meet up
    
    	* Dashboard html structure
    	* css design for the structure
    	* Create meet up button
    	* Delete meet up button
    		- Create meet up form
    	* Delete meet up modal window
    		- Delete meet up form
    
    Closed #162662353

---
+++ dist/js/script.js
@@ -0,0 +1,57 @@
+/**
+ * Meet up modal
+ */
+
+(function() {
+	// Get the  create-meetup-modal modal
+	const createMeetupModal = document.getElementById('create-meetup-modal');
+
+	// Get the delete-meetup-modal modal
+	const deleteMeetupModal = document.getElementById('delete-meetup-modal');
+
+	// Get the button that opens the modal
+	const createMeetupbtn = document.getElementById('create-meetup');
+  
+	// Get the button that opens the modal
+	const deleteMeetupbtn = document.getElementById('delete-meetup');
+  
+	// Get the <span> element that closes the modal
+	const span = document.getElementsByClassName('close')[0];
+  
+	// Get the <span> element that closes the modal
+	const close = document.getElementsByClassName('close')[1];
+  
+	// When the user clicks on the button, open the modal
+	createMeetupbtn.addEventListener('click', () => {
+		createMeetupModal.style.display = 'block';
+	});
+  
+	// When the user clicks on the button, open the modal
+	deleteMeetupbtn.addEventListener('click', () => {
+		deleteMeetupModal.style.display = 'block';
+	});
+
+	// When the user clicks on <span> (x), close the modal
+	span.addEventListener('click', () => {
+		createMeetupModal.style.display = 'none';
+	});
+  
+	// When the user clicks on <span> (x), close the modal
+	close.addEventListener('click', () => {
+		deleteMeetupModal.style.display = 'none';
+	});
+  
+	// When the user clicks anywhere outside of the modal, close it
+	window.addEventListener('click', event => {
+		if (event.target == createMeetupModal) {
+			createMeetupModal.style.display = 'none';
+		}
+	});
+  
+	// When the user clicks anywhere outside of the modal, close it
+	window.addEventListener('click', event => {
+		if (event.target == deleteMeetupModal) {
+			deleteMeetupModal.style.display = 'none';
+		}
+	});
+})();
